25ae7cf1ad4adcd3a45320a3677c0f705da7f890
Mikko Pasanen <pasanemikko@gmail.com>
2023-04-12 22:40:50 +0300

    Patch 2.6.4

--- Pint Rush/Assets/Scripts/Management/GameManagement.cs
+++ Pint Rush/Assets/Scripts/Management/GameManagement.cs
@@ -19,6 +19,7 @@ namespace PintRush
         [SerializeField] TextMeshProUGUI totalPointText;
 
         [SerializeField] private Life life;
+        [SerializeField] private CustomerSpawnController csc;
 
         [SerializeField] private bool developerMode;
         [SerializeField] private bool allowSpawn;
@@ -29,8 +30,16 @@ namespace PintRush
         private int beerTwo;
         private int beerThree;
 
+        //Playerprefs
+        private string playerPrefKey = "HowToPlay";
+        private bool defaultValue = true;
+        [SerializeField] private GameObject checkMark;
+        [SerializeField] private GameObject checkMarkTwo;
+
         private void Start()
         {
+            bool showHowToPlay = PlayerPrefs.GetInt(playerPrefKey, defaultValue ? 1 : 0) == 1;
+
             gameOverScreen.SetActive(false);
             beerOne = 0;
 
@@ -53,7 +62,18 @@ namespace PintRush
                 upgradePointsText.text = $"{upgradePoints}";
                 totalPointText.text = $"{totalPoints}";
             }
-            howToPlay.SetActive(true);
+
+            if(showHowToPlay)
+            {
+                howToPlay.SetActive(true);
+                checkMarkTwo.SetActive(true);
+
+            }
+            else
+            {
+                csc.StartSpawn();
+                checkMarkTwo.SetActive(false);
+            }
         }
 
         public bool GetMuteState()
@@ -161,6 +181,53 @@ namespace PintRush
         public bool CheckAllowSpawn()
         {
             return allowSpawn;
+        }
+
+        public void SetShowHowToPlay()
+        {
+            defaultValue = !defaultValue;
+            PlayerPrefs.SetInt(playerPrefKey, defaultValue ? 1 : 0);
+            PlayerPrefs.Save();
+
+            if(defaultValue)
+            {
+                checkMark.SetActive(true);
+                Debug.Log("SHOW DEM OHJEET");
+                checkMarkTwo.SetActive(true);
+            }
+            else if(!defaultValue)
+            {
+                checkMark.SetActive(false);
+                Debug.Log("DONT SHOW DEM OHJEET");
+                checkMarkTwo.SetActive(false);
+            }
+        }
+
+        public void ShowHowToPlay()
+        {
+            /*
+            defaultValue = true;
+            PlayerPrefs.SetInt(playerPrefKey, defaultValue ? 1 : 0);
+            PlayerPrefs.Save();
+            Debug.Log("show ohjeet ysdrtfd sw");
+            checkMarkTwo.SetActive(true);
+            */
+
+            defaultValue = !defaultValue;
+            PlayerPrefs.SetInt(playerPrefKey, defaultValue ? 1 : 0);
+            PlayerPrefs.Save();
+
+            if (defaultValue)
+            {
+                Debug.Log("SHOW DEM OHJEET");
+                checkMarkTwo.SetActive(true);
+            }
+            else if (!defaultValue)
+            {
+                Debug.Log("DONT SHOW DEM OHJEET");
+                checkMarkTwo.SetActive(false);
+            }
+
         }
     }
 }
